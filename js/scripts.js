eac34179b475954d219f6c8ea29ec8819db4b1c6
MyselfRebel <56184411+MyselfRebel@users.noreply.github.com>
2024-08-02 22:33:57 +0530

    done

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    
+    $('.nav-toggle').click(function() {
+        $('.main-nav').toggleClass('is-open');
+        $('.hamburger').toggleClass('is-open');
+    })
+})
